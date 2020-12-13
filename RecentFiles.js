c24690b71c12c586b75f463185f58059b9ec9885
Rodrigues <maot.rodrigues@gmail.com>
2020-12-13 03:35:28 -0300

    [GREEN] Remove the last element if limit length has been reached

--- RecentFiles.js
+++ RecentFiles.js
@@ -3,11 +3,12 @@ module.exports = function() {
     const maxFiles = 10
 
     const add = (file) => {
-        if (list.length == maxFiles) return
-            
         for (let i = 0; i < list.length; i++)
             if (JSON.stringify(list[i]) === JSON.stringify(file))
                 list.splice(i, 1);
+        if (list.length == maxFiles){
+            list.pop()
+        }
         list.unshift(file)
     }
 
