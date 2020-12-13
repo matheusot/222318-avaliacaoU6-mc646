645b4619147ec2ea42697f2cecd5160c9ee5a004
Rodrigues <maot.rodrigues@gmail.com>
2020-12-13 03:52:02 -0300

    [GREEN] enableUpdate method implemented
    Also added an if statement to add to check if we can update the array or not

--- RecentFiles.js
+++ RecentFiles.js
@@ -1,22 +1,30 @@
 module.exports = function() {
     const list = []
     const maxFiles = 10
+    let update = true
 
     const add = (file) => {
-        for (let i = 0; i < list.length; i++)
+        if (update) {
+            for (let i = 0; i < list.length; i++)
             if (JSON.stringify(list[i]) === JSON.stringify(file))
                 list.splice(i, 1);
-        if (list.length == maxFiles) list.pop()
-        list.unshift(file)
+            if (list.length == maxFiles) list.pop()
+            list.unshift(file)
+        }
     }
 
     const clear = () => {
         list.length = 0
     }
 
+    const enableUpdate = (value) => {    
+        update = value
+    }
+
     return Object.freeze({
         add,
         clear,
+        enableUpdate,
         list
     })
 }
