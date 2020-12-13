2ef311d49246290599c8dc0c7e310ecd4b45ccae
Rodrigues <maot.rodrigues@gmail.com>
2020-12-13 02:30:44 -0300

    [RED] Should return array[0] = proj1 and length = 1
    Nothing could be refactored between last green state and this red state.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 Lista de Arquivos Recentes:
 + Quando o programa está executando pela primeira vez, essa lista está vazia;
-- Quando um arquivo é aberto, ele é adicionado à lista de arquivos recentes;
++ Quando um arquivo é aberto, ele é adicionado à lista de arquivos recentes;
 - Se um arquivo aberto já está na lista, este é levado ao topo da lista, sem conter itens duplicados na lista
 - Se a lista alcança seu limite (normalmente de 10 a 15 itens na lista), o item mais antigo é removido quando um novo item é adicionado.
 - A lista pode ser esvaziada a qualquer momento
@@ -38,22 +38,33 @@ describe('tests', function() {
 
         it('Should return array with first position equal to object {"name": "Project1.js", "dateOpened": "2020-12-13 02:04:34"}', function() {
             const testObject = new RecentFiles()
-            const file = {name: "Project1.js", dateOpened: "2020-12-13 02:04:34"};
+            const file = {name: "Project1.js", dateOpened: "2020-12-13 02:04:34"}
             testObject.add(file)
             assert.strictEqual(testObject.list[0], file)
         })
 
         it('Should return array[0] = proj3, array[1] = proj2, array[0] = proj1', function() {
             const testObject = new RecentFiles()
-            const proj1 = {name: "Project1.js", dateOpened: "2020-12-13 02:04:34"};
-            const proj2 = {name: "Project2.js", dateOpened: "2020-12-13 02:21:43"};
-            const proj3 = {name: "Project3.js", dateOpened: "2020-12-13 02:23:21"};
+            const proj1 = {name: "Project1.js", dateOpened: "2020-12-13 02:04:34"}
+            const proj2 = {name: "Project2.js", dateOpened: "2020-12-13 02:21:43"}
+            const proj3 = {name: "Project3.js", dateOpened: "2020-12-13 02:23:21"}
             testObject.add(proj1)
             testObject.add(proj2)
             testObject.add(proj3)
             assert.strictEqual(testObject.list[0], proj3)
             assert.strictEqual(testObject.list[1], proj2)
             assert.strictEqual(testObject.list[2], proj1)
+        })
+    })
+
+    describe('addDuplicateFilesToObject', function() {
+        it('Should return array[0] = proj1 and length = 1', function() {
+            const testObject = new RecentFiles()
+            const proj1 = {name: "Project1.js", dateOpened: "2020-12-13 02:04:34"}
+            testObject.add(proj1)
+            testObject.add(proj1)
+            assert.strictEqual(testObject.list.length, 1)
+            assert.strictEqual(testObject.list[0], proj1)
         })
     })
 
