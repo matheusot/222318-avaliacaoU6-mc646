87f6594abfcc21c15d84b10d2676ee5139747413
Rodrigues <maot.rodrigues@gmail.com>
2020-12-13 03:23:45 -0300

    [REFACTOR] Brackets for if and for not needed anymore. List can be const

--- RecentFiles.js
+++ RecentFiles.js
@@ -1,13 +1,10 @@
 module.exports = function() {
-    let list = []
+    const list = []
 
     const add = (file) => {
-        for (let i = 0; i < list.length; i++) {
-            if (JSON.stringify(list[i]) === JSON.stringify(file)) {
+        for (let i = 0; i < list.length; i++)
+            if (JSON.stringify(list[i]) === JSON.stringify(file))
                 list.splice(i, 1);
-            }
-        }   
-            
         list.unshift(file)
     }
 
