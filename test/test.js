bb1d6c5da6580129a7c0154eefa9ba0f52a1877a
Rodrigues <maot.rodrigues@gmail.com>
2020-12-13 04:00:00 -0300

    Last test added, runned on first try. Implementation finished

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ Lista de Arquivos Recentes:
 + Se um arquivo aberto já está na lista, este é levado ao topo da lista, sem conter itens duplicados na lista
 + Se a lista alcança seu limite (normalmente de 10 a 15 itens na lista), o item mais antigo é removido quando um novo item é adicionado.
 + A lista pode ser esvaziada a qualquer momento
-- A atualização da lista pode ser desabilitada/habilitada. Caso seja desabilitada, os arquivos já existentes ficarão na lista, mas não serão adicionados novos arquivos.
++ A atualização da lista pode ser desabilitada/habilitada. Caso seja desabilitada, os arquivos já existentes ficarão na lista, mas não serão adicionados novos arquivos.
 
 3 principais regras do TDD:
 - Write production code only to pass a failing unit test.
@@ -140,6 +140,23 @@ describe('tests', function() {
             for (let i = 0; i < 100; i++)
                 testObject.add({name: `Proj${i}.js`, dateOpened: "2020-12-13 02:04:34"})
             assert.strictEqual(testObject.list.length, 3)
+        })
+
+        it('Should have length=3 list[0]=proj1 after re-enabling list update', function() {
+            const testObject = new RecentFiles()
+            const proj1 = {name: "Project1.js", dateOpened: "2020-12-13 02:04:34"}
+            const proj2 = {name: "Project2.js", dateOpened: "2020-12-13 02:21:43"}
+            const proj3 = {name: "Project3.js", dateOpened: "2020-12-13 02:23:21"}
+            testObject.add(proj1)
+            testObject.add(proj2)
+            testObject.enableUpdate(false)
+            for (let i = 0; i < 100; i++)
+                testObject.add({name: `Proj${i}.js`, dateOpened: "2020-12-13 02:04:34"})
+            testObject.enableUpdate(true)
+            testObject.add(proj3)
+            testObject.add(proj1)
+            assert.strictEqual(testObject.list.length, 3)
+            assert.strictEqual(testObject.list[0], proj1)
         })
     })
 
