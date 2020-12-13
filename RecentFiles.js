4653df32a2269181d474a58ef5d76344305aae98
Rodrigues <maot.rodrigues@gmail.com>
2020-12-13 03:59:34 -0300

    [REFACTOR] Updated if statement of add method
    Also made clear and enableUpdate methods one liners and fixed minor indentation issues

--- RecentFiles.js
+++ RecentFiles.js
@@ -4,27 +4,18 @@ module.exports = function() {
     let update = true
 
     const add = (file) => {
-        if (update) {
-            for (let i = 0; i < list.length; i++)
+        if (!update) return
+        for (let i = 0; i < list.length; i++)
             if (JSON.stringify(list[i]) === JSON.stringify(file))
                 list.splice(i, 1);
-            if (list.length == maxFiles) list.pop()
-            list.unshift(file)
-        }
+        if (list.length == maxFiles) list.pop()
+        list.unshift(file)
     }
 
-    const clear = () => {
-        list.length = 0
-    }
-
-    const enableUpdate = (value) => {    
-        update = value
-    }
+    const clear = () => list.length = 0
+    const enableUpdate = (value) => update = value
 
     return Object.freeze({
-        add,
-        clear,
-        enableUpdate,
-        list
+        add, clear, enableUpdate, list
     })
 }
